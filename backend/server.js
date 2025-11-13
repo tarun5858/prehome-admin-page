75e79cc749cb64e61d61c3f51b5ad7ac4d852339
tarun5858 <ts460000@gmail.com>
2025-11-13 18:03:47 +0530

    latest commit

--- backend/server.js
+++ backend/server.js
@@ -97,7 +97,6 @@ app.use((req, res, next) => {
         // Fallback check using referer header
         res.setHeader('Access-Control-Allow-Origin', req.header('referer'));
     }
-
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
